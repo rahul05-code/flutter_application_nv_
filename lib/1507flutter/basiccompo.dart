a46b1d29c5f95477377aff2d508fdf121e7c7c00
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-15 13:36:44 +0530

    test

--- lib/1507flutter/basiccompo.dart
+++ lib/1507flutter/basiccompo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_nv/1507flutter/textstyleexam.dart';
 
 class ContainerExample extends StatelessWidget {
   const ContainerExample({super.key});
@@ -14,7 +15,7 @@ class ContainerExample extends StatelessWidget {
             color: Colors.amber,
             child: const Padding(
               padding: EdgeInsets.all(20),
-              child: Text("Hello flutter!"),
+              child: Text("Hello flutter!", style: CustomTextStyle.Style1),
             ),
           ),
         ),
---
+++ lib/1507flutter/textstyleexam.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static const TextStyle Style1 = TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+  );
+
+  static const TextStyle Style2 = TextStyle(
+    fontSize: 24,
+    color: Colors.red,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+  );
+
+  static const TextStyle Style3 = TextStyle(
+    fontSize: 24,
+    color: Colors.cyan,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+  );
+
+  static const TextStyle Style4 = TextStyle(
+    fontSize: 24,
+    color: Colors.yellow,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+  );
+
+  static const TextStyle Style5 = TextStyle(
+    fontSize: 24,
+    color: Colors.green,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+  );
+  
+
+}
